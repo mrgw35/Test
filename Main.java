ffe3d44d50ac1de89a55fdf099ec267f2e633525
Max Rogowski <maxrogowski22@outook.de>
2022-12-07 12:12:49 +0100

    Added new SOUT-Line

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hallo ich bin die zweite Zeile in der Main Klasse");
     }
 }
